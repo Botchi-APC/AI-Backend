03b2f3b07736510ca587dc8c0c5ebe2a470135e0
chxe01 <a01082311887l@gmail.com>
2026-06-08 23:13:04 +0900

    fix: localhost 경로 수정

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.survey import SurveyRequest
 from services.scoring import calculate_score
 from services.personality import determine_type
 from services.ai_agent import generate_ai_report
+from data.questions import QUESTIONS
+
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5175",  # Vite 기본
+]
 
-@app.post("/analyze")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/api/analyze")
 def analyze(data: SurveyRequest):
 
     if len(data.answers) != 25:
-        raise HTTPException(400, "25개 필요")
+        raise HTTPException(status_code=400, detail="25개 필요")
 
     scores = calculate_score(data.answers)
     personality_type = determine_type(scores)
@@ -28,3 +43,7 @@ def analyze(data: SurveyRequest):
         "scores": scores,
         "report": ai_report
     }
+
+@app.get("/questions")
+def get_questions():
+    return QUESTIONS
